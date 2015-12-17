53aa70f151884d39ba93e32ffb545e47b6704fa2
MOSOC <warshightide@gmail.com>
2015-12-17 15:11:55 +0000

    CDN, Route, and Hello World

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 var app = angular.module('futaba-clone', ['ngRoute', 'firebase']);
+
+//using ngRoute
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            controller: 'BoardController',
+            templateUrl: 'board.html'
+        })
+//        .otherwise({redirectTo: '/'})
+});
+
+app.controller("BoardController", function($scope, $firebaseObject) {
+
+});
